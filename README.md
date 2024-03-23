d84e820cd33af26ac4415c29ac3f38c0b916aad9
clmiranda <miranda76575@gmail.com>
2024-03-23 14:11:06 -0400

    Added Colorama Library to set color to the different results

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # Python Click
 
 This application use the [Python Click Library](https://pypi.org/project/click/) to create a Command Line Interface (CLI) to perform user CRUD operations on a Json file.
+
+Use of [Pretty Table Library](https://pypi.org/project/prettytable/) and [Colorama Library](https://pypi.org/project/colorama/) to to display data in tables in an attractive ASCII format and set color to the different results.
+
+Example of list of users:
+
+![Example of list of users](img/users.png)
+
+Example of added user:
+
+![Example of added user](img/added.png)
+
+Example of updated user:
+
+![Example of updated user](img/updated.png)
+
+Example of deleted user:
+
+![Example of deleted user](img/deleted.png)
--- cli.py
+++ cli.py
@@ -1,12 +1,12 @@
 import json_operations
 import click
-from prettytable import PrettyTable
 from prettytable.colortable import ColorTable, Themes
+from colorama import init, Fore, Back, Style
 
 
-table = PrettyTable()
 table = ColorTable(theme=Themes.OCEAN)
 table.field_names = ["id", "name", "lastname"]
+init(autoreset=True)
 
 
 @click.group()
@@ -21,15 +21,19 @@ def cli():
 def new(context, name, lastname):
     users = json_operations.read_json()
     if not name or not lastname:
-        context.fail("Name and Lastname are required")
+        context.fail(Style.BRIGHT + Fore.RED + "Name and Lastname are required")
     elif json_operations.search_name_repeated(name) >= 1:
-        context.fail("User already exists")
+        context.fail(Style.BRIGHT + Fore.RED + "User already exists")
     else:
         new_id = len(users) + 1
         new_user = {"id": new_id, "name": name, "lastname": lastname}
         users.append(new_user)
         json_operations.write_json(users)
-        print(f"User {name} {lastname} has been created successfully with id: {new_id}")
+        print(
+            Style.BRIGHT
+            + Fore.GREEN
+            + f"User {name} {lastname} has been created successfully with id: {new_id}",
+        )
 
 
 @cli.command()
@@ -45,9 +49,13 @@ def user(id):
     data = json_operations.read_json()
     user = next((i for i in data if i["id"] == id), None)
     if user is None:
-        print(f"User with id {id} not found")
+        print(Style.BRIGHT + Fore.RED + f"User with id {id} not found")
     else:
-        print(f"{user['id']} - {user['name']} - {user['lastname']}")
+        print(
+            Style.BRIGHT
+            + Fore.GREEN
+            + f"{user['id']} - {user['name']} - {user['lastname']}"
+        )
 
 
 @cli.command()
@@ -64,7 +72,9 @@ def update(id, name, lastname):
                 user["lastname"] = lastname
             break
     json_operations.write_json(data)
-    print(f"User with id {id} has been updated successfully")
+    print(
+        Style.BRIGHT + Fore.GREEN + f"User with id {id} has been updated successfully"
+    )
 
 
 @cli.command()
@@ -73,11 +83,15 @@ def delete(id):
     data = json_operations.read_json()
     user = next((i for i in data if i["id"] == id), None)
     if user is None:
-        print(f"User with id {id} not found")
+        print(Style.BRIGHT + Fore.RED + f"User with id {id} not found")
     else:
         data.remove(user)
         json_operations.write_json(data)
-        print(f"User with id {id} has been deleted successfully")
+        print(
+            Style.BRIGHT
+            + Fore.GREEN
+            + f"User with id {id} has been deleted successfully"
+        )
 
 
 if __name__ == "__main__":
